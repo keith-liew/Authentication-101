8908075b18c7a38c5c5437b64968c0963c18ac12
keith-liew <>
2022-10-06 22:56:50 +0800

    Add route configuration
    User register and user login route and then redirect to secrets page

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    secrets: [String]
+    secrets: [String],
+    password: String,
 });
 
 module.exports = mongoose.model("User",userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,61 @@
 const router = require("express").Router();
-
+const User = require("../models/User");
 //route setting
 // router.use("/users", require("./users"))
 
-router.get("/", (req,res)=> {
+router.get("/", (req, res) => {
     res.render("home");
 });
 
-router.get("/login", (req,res)=>{
+router.get("/login", (req, res) => {
     res.render("login");
 });
 
-router.get("/register", (req,res)=>{
+router.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ username: username })
+        .then((user) => {
+            //if user not found
+            if (!user) {
+                res.status(400).json({ success: false, msg: "User not found" });
+            }
+            //user found
+            //and check user password
+            if (user.password === password) {
+                res.status(200).render("secrets");
+            } else {
+                res.status(401).json({ success: false, msg: "Unauthorized user" })
+            }
+        })
+});
+
+router.get("/register", (req, res) => {
     res.render("register");
 });
 
-router.get("/secrets", (req,res)=>{
+router.post("/register", (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save()
+        .then(() => {
+            //render secret page
+            res.status(201).render("secrets");
+        })
+        .catch((err) => {
+            res.status(404).json({ sucess: false, msg: err.message });
+        })
+});
+
+router.get("/secrets", (req, res) => {
     res.render("secrets");
 });
 
-router.get("/submit", (req,res)=>{
+router.get("/submit", (req, res) => {
     res.render("submit");
 });
 
