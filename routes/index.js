a14ced647547f4515047010ecc8da817d4ec1ecf
keith-liew <>
2022-10-07 16:48:08 +0800

    Add bcrypt package
    Use bcrypt to generate more complex hash, and use its function to verify back the user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-
+const User = require("../models/User")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 //route setting
 // router.use("/users", require("./users"))
 
@@ -11,8 +13,52 @@ router.get("/login", (req, res) => {
     res.render("login");
 });
 
+router.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ username: username })
+        .then((user) => {
+            //if user not found
+            if (!user) {
+                res.status(400).json({ success: false, msg: "User not found" });
+            }
+            //user found
+            //and check user password
+            bcrypt.compare(password, user.hash, function (err, result) {
+                // result == true
+                if (result) {
+                    res.status(200).render("secrets");
+                } else {
+                    res.status(401).json({ success: false, msg: "Unauthorized user" })
+                }
+            });
+        })
+});
+
 router.get("/register", (req, res) => {
     res.render("register");
+});
+
+router.post("/register", (req, res) => {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(req.body.password, salt, function (err, hash) {
+            // Store hash in your password DB.
+            const newUser = new User({
+                username: req.body.username,
+                hash: hash
+            })
+
+            newUser.save()
+                .then(() => {
+                    //render secret page
+                    res.status(201).render("secrets");
+                })
+                .catch((err) => {
+                    res.status(404).json({ sucess: false, msg: err.message });
+                })
+        });
+    });
 });
 
 router.get("/secrets", (req, res) => {
