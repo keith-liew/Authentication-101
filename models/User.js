f97013a6855ca268f9fb29d647cc5223b67f84fe
keith-liew <>
2022-10-07 13:11:59 +0800

    Add mongoose-encryption module
    It will handle data encryption when save and data decryption when retrive

--- models/User.js
+++ models/User.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    secrets: [String]
+    secrets: [String],
+    password: String
 });
+
+//moogoose will handle the password encryption and decryption itself
+//simplfied encryption with just one key string as SECRET, default options is { encryptionKey: encKey, signingKey: sigKey }
+//selected <password> as the only encryption field
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 module.exports = mongoose.model("User", userSchema);
